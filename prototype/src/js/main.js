cc08afdb2733b10d619a1b8c509822f47232e970
Wiebe Geus <wiebegeus@gmail.com>
2025-11-20 14:49:21 +0100

    checkout.html en shop.html af index.html gefinetuned

--- prototype/src/js/main.js
+++ prototype/src/js/main.js
@@ -1,4 +1,9 @@
 import { loadComponent } from "./loadComponents.js";
+import { initShop } from "./shop.js";
+import { initCheckout } from "./checkout.js";
 
 await loadComponent('navbar', './src/components/navbar.html');
-await loadComponent('footer', './src/components/footer.html')
+await loadComponent('footer', './src/components/footer.html');
+
+initShop();
+initCheckout();
